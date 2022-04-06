1c794f79dda5cbec53867645fba1d8525f12afd3
Jesus <jgarcia28599@gmail.com>
2022-04-06 16:18:33 -0300

    Primer commit - Creacion de primeros componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ProyectsComponent } from './proyects/proyects.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutMeComponent,
+    ProyectsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
